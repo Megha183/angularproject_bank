d2d46f3073769420bc525e152668a3b761289f1c
unknown <meghabhaskar183@gmail.com>
2023-02-01 17:53:55 +0530

    fourth commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -12,24 +14,17 @@ export class LoginComponent {
   acno=''
   // acno:any
   psw=''
-userDetails:any={
 
-  
-  1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-  1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-  1002:{acno:1002,username:"arun",password:"abc123",balance:0},
-  1003:{acno:1003,username:"akil",password:"abc123",balance:0}
-
-}
-constructor(){ }
+constructor(private router:Router,private ds:DataService){ }
 
 login(){
   var acnum=this.acno
   var psw=this.psw
-  var userDetails=this.userDetails
+  var userDetails=this.ds.userDetails
   if(acnum in userDetails){
 if(psw==userDetails[acnum]["password"]){
   alert("login success")
+  this.router.navigateByUrl(`dashboard`)
 }else{
   alert("incorrect passsword")
 }
